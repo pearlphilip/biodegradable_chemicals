c3480cbf5476829693d52ddc3f4aab875826fc89
Pearl Philip <pearlphilip007@gmail.com>
2016-02-26 09:15:59 -0800

    Added docstring

--- format_data.py
+++ format_data.py
@@ -2,13 +2,15 @@ import numpy as np
 import pandas as pd
 import os
 
-#This Function is used to format the dataset
+
 def read_data(filename):
+    """
+    This Function is used to format the dataset
+    """
     if os.path.exists(filename):
-        data = pd.read_csv(filename,sep=";")
-        print("Showing the first 5 lines of %s" % (filename))
+        data = pd.read_csv(filename, sep=";")
+        print("Showing the first 5 lines of %s" % filename)
         print(data.head())
         return data
     else:
         raise FileNotFoundError(filename)
-    return None
