14878f64fe9a4c108fd5f74fb3463f755cfbadb6
Pearl Philip <pearlphilip007@gmail.com>
2016-05-19 11:50:03 -0700

    Optimized imports

--- driver.py
+++ driver.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python2.7
-'''
-Main driver for running the tool
-'''
 
-import pandas as pd
+"""
+Main driver for running the tool
+"""
 
 import format_data
 import nn_model
 import transform
 
+
 def main():
-    '''
+    """
     Entry point for all code
-    '''
+    """
+
     print("starting up")
     df = format_data.read_data('data/biodeg.csv')
     df = transform.transform(df, 'class')
